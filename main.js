829f3c10cbb52f03ba5e24cd908c7c5408ceaa76
D4rk4ns <jonathanlsr9611@gmail.com>
2022-10-25 17:48:40 -0400

    Testing something

---
+++ main.js
@@ -0,0 +1,45 @@
+const cards = document.getElementById("cards");
+const cardList = document.getElementsByClassName("card");
+
+
+function handleMouseMove(card,e){
+    const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+}
+
+cards.onmousemove = (e) => {
+    for (const card of cardList) handleMouseMove(card,e) 
+};
+
+
+var swiper = new Swiper(".LolSwiper", {
+    slidesPerView: 1,
+    centeredSlides: false,
+    slidesPerGroupSkip: 1,
+    grabCursor: true,
+    keyboard: {
+      enabled: true,
+    },
+    breakpoints: {
+      769: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+      },
+    },
+    scrollbar: {
+      el: ".swiper-scrollbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
